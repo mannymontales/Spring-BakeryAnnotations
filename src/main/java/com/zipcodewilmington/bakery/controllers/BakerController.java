9630f791bfac7e6e72410b6edc2df499cd079a5c
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-08-18 14:23:24 -0400

    completed lab and passed all tests, and testing through postman works

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,12 +2,17 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/bakers")
 public class BakerController {
     private BakerService service;
 
+    @Autowired
     public BakerController(BakerService service) {
         this.service = service;
     }
@@ -16,19 +21,24 @@ public class BakerController {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> show(Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Baker> show(@PathVariable("id") Long id) {
+        //can also write this without an id
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> create(Baker baker) {
+    @PostMapping
+    public ResponseEntity<Baker> create(@RequestBody Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Baker> update(@PathVariable Long id, @RequestBody Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
